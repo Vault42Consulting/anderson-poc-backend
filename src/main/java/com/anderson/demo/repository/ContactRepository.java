8e4b19dd7488a96ff8ba494c167a338dbd28ebd1
Ryan Duffield <ryan@ryanduffield.me>
2024-12-13 04:13:20 -0800

    Re-add "Add very basic sorting"
    This reverts commit 49233add206609a2a6a897676d9914e6f1e3c578.

--- src/main/java/com/anderson/demo/repository/ContactRepository.java
+++ src/main/java/com/anderson/demo/repository/ContactRepository.java
@@ -1,9 +1,17 @@
 package com.anderson.demo.repository;
 
 import com.anderson.demo.model.Contact;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface ContactRepository extends JpaRepository<Contact, String> {
-    List<Contact> findByUserId(String userId);
+    @Query(value = "SELECT * FROM contacts WHERE user_id = ?1 " +
+            "ORDER BY COALESCE(SPLIT_PART(name, ' ', -1), name)", nativeQuery = true)
+    List<Contact> findByUserIdOrderedByLastName(String userId);
+
+    @Query(value = "SELECT * FROM contacts WHERE user_id = ?1 ORDER BY name", nativeQuery = true)
+    List<Contact> findByUserIdOrderedByFullName(String userId);
 }
